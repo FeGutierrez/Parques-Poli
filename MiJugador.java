cd19af0c6a2ad1abb8564d870deb656670dee2d0
Fellhipe <fgutierrezf@unal.edu.co>
2020-10-08 21:50:27 -0500

    Cuando hay pares especiales (6s o 1s) Y hay piezas en la carcel sacamos las piezas de la carcel y determinamos si habia piezas validas (cuando sale SOLO una pieza de la carcel) para distribuir los puntajes, en caso de que no haya piezas validas, se mueve una de las piezas que acaba de salir de la carcel con el valor de dados[0], minimo de un arreglo, pertenencia a un arreglo, prototipo de mover, mover despues de carcel, de posibilidad de asegurar, posibilidad de capturar, sacar maximo dos piezas de la cárcel, sacar todas las piezas de la cárcel, determinar las piezas validas para mover en cualquier momento, cuantas piezas han salido del juego

--- MiJugador.java
+++ MiJugador.java
@@ -1,3 +1,4 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.time.chrono.MinguoDate;
 import java.util.ArrayList;
 import java.util.Random;
@@ -47,52 +48,13 @@ public class MiJugador extends Jugador {
 	* */
 
 	int[][] piezas = new int[4][4]; //Matriz de las piezas
-	int[] dados; // arreglo para dados
+	int[][] diferenciaConFinal = new int[4][4];
+	int[] puntajes; // arreglo para dados
 	int paresAcumulados; //pares acumulados
 	final int[] carceles = {-4, -21, -38, -55};
 	int turnoInicial=-1;
+	int contadorTurnos;
 	int[] casillasSeguras = {4, 11, 16, 21, 28, 33, 38, 45, 50, 55, 62, 67}; //Solo funciona con 4 jugadores
-
-	public boolean verificarLanzamientos( int[] lanzamientosJugadores ){
-		boolean valido = true;
-		for(int i = 0; i < lanzamientosJugadores.length; ++i){
-			if(lanzamientosJugadores[i] == 0){
-				valido = false;
-			}
-		}
-		return valido;
-	}
-
-	public int[] elMayor( int[] arreglo){
-		int[] resultados = {0,-1}; // resultados[0] = max del arreglo resultados[1] = indice del max
-		int max =0;
-		for(int i = 0; i < arreglo.length; ++i){
-			if ( arreglo[i] > max) {
-				max = arreglo[i];
-				resultados[0] = max;
-				resultados[1] = i;
-			}
-		}
-		return resultados;
-	}
-
-	public ArrayList<Integer> maxRepetido(int[] lanzamientosJugadores, int maximo, int indiceDelMax ){ //equivalente a Hay dos o mas valores iguales
-		ArrayList<Integer> indicesEmpatados = new ArrayList<>();
-		indicesEmpatados.clear(); //Lista vacia
-		boolean anadirIndiceMax = false;
-		for(int i = 0; i < lanzamientosJugadores.length; ++i){
-			if(i != indiceDelMax){
-				if(lanzamientosJugadores[i] == maximo){
-					indicesEmpatados.add(i);
-					anadirIndiceMax = true;
-				}
-			}
-		}
-		if(anadirIndiceMax){
-			indicesEmpatados.add(indiceDelMax);
-		}
-		return indicesEmpatados;
-	}
 
 	public void inicializar(){
 		//(1) INICIO DEL JUEGO
@@ -101,6 +63,12 @@ public class MiJugador extends Jugador {
 				piezas[i][j] = carceles[i];
 			}
 		} // Fin de (1)
+		//INICIALIZAR MATRIZ AUXILIAR PARA VER DISTANCIAS HASTA LA META
+		for(int i = 0; i < 4; ++i){
+			for( int j = 0; j< 4; ++j){
+				diferenciaConFinal[i][j] = 75 - (carceles[i]);
+			}
+		}
 		// (2) -> dados
 		Random generador = new Random();
 		int[] lanzamientosJugadores = new int[4];
@@ -135,6 +103,7 @@ public class MiJugador extends Jugador {
 			index = info[1];
 			indicesEmpatados = maxRepetido(lanzamientosJugadores, max, index);
 			turnoInicial = index;
+			contadorTurnos = index;
 
 		}
 		//int numeroJugadores = 4;
@@ -150,15 +119,23 @@ public class MiJugador extends Jugador {
 	}
 	
 	public int[] hacerJugada(int[][] piezas, int[] dados, int pares_acumulados) { //Asumimos que los elementos
+		int turnoActual = contadorTurnos % 4; // turnos cíclicos en el rango {0, 1, 2 ,3}
+		puntajes = dados;
 		// han sido inicializados de forma correcta previamente
-		int turno = turnoInicial; //el turno inicial debe ser el indice obtenido al momento de inicializar el juego
-		int totalEncarceladas = cuantasEnPrision(piezas[turno], turno);
+		//el turno inicial debe ser el indice obtenido al momento de inicializar el juego
+		int totalEncarceladas = cuantasEnPrision(piezas[turnoActual], turnoActual);
+
+		//
 		if(dados[0] == dados[1]){ //Salió par, decidir si mover o salir de prisión
+
 			//Para considerar salir de prisión, debo estar en prisión...
 			if (totalEncarceladas == 0) { //No hay nada que decidir
-				//mover();//
-			} else { //Decidir
+				ArrayList<Integer> validas = piezasValidos(piezas[turnoActual], turnoActual);
+				++paresAcumulados; //Se incrementan los pares acumulados cuando no se saca de la cárcel
+				mover(validas);//
+			} else { //Decidir porque hay piezas en la carcel
 				/*
+				* Los pares especiales de 6s y 1s siempre serán prioritarios para sacar de la cárcel
 				* Bajo que circunstancias es mejor mover una pieza a sacar de la cárcel?:
 				* 	- Si hay una pieza en una casilla que no es segura
 				* 		Prioridad: La pieza más adelantada
@@ -168,31 +145,100 @@ public class MiJugador extends Jugador {
 				* En otro caso es recomendable sacar de la cárcel
 				*
 				* */
+				if( dados[0] == 6 || dados[0] == 1) { //Caso de pares especiales
+					ArrayList<Integer> validas = piezasValidos(piezas[turnoActual], turnoActual); //Aqui tenemos las piezas que no estan en la carcel
+					//pero tampoco han salido del juego, en caso de que esta lista sea vacía, se debe mover con una pieza salida de la carcel
+					ArrayList<Integer> salidas = sacarTodasDeLaCarcel( piezas[turnoActual], turnoActual); //Hasta aquí saca a todas las piezas de la cárcel
+					// Tenemos una lista que indica cuales piezas salieron para mover con ellas en caso de que no haya
+					//mas piezas para hacer un movimiento
+					if( salidas.size() == 1){ //Si solo habia una en la cárcel
+						//Verificar si hay más piezas para distribuir el puntaje
+						if( !validas.isEmpty()){ //Hay al menos una pieza que no estaba en la cárcel
+							//y tampoco había salido del juego
+							//Se mueve con una de esas piezas validas
+							moverPostCarcel( validas);
+						} else {
+							moverPostCarcel( salidas);
+						}
+					} else { //Hay más de una pieza en la cárcel, no hay movimientos adicionales
 
-			}
-
-		}
-
-
-		if(dados[0] == dados[1]) { //Hay par
-
-			if (dados[0] == 6 || dados[0] == 1) { //se sacan todas las piezas de la carcel
-				if (totalEncarceladas == 0) {
-					//mover();//
-				} else {
-					for(int i=0; i<piezas[turno].length; ++i){
-						piezas[turno][i] = -carceles[turno]; //Casilla inicial
+					}
+				} else { //Fin de los pares especiales (6s o 1s) pero si tenemos pieza(s) en la cárcel
+					ArrayList<Integer> piezasJugables = piezasValidos( piezas[turnoActual], turnoActual);
+					//Vemos si hay piezas que puedan mover antes de sacar de la carcel
+					ArrayList<Integer> piezasSalidas = sacarMaxDosDeLaCarcel( piezas[turnoActual], turnoActual );
+					if( !piezasJugables.isEmpty() ){ //Habia piezas validas para distribuir el puntaje?
+						moverPostCarcel(piezasJugables);
+					} else { //Esto quiere decir que debemos mover con una de las piezas salidas de la carcel
+						moverPostCarcel(piezasSalidas);
 					}
 				}
-			}
-			//En caso de que el par no sea de 6s o 1s (2, 3, 4, 5)
 
+			}
+			--turnoActual;
 		}
 
+
+		++turnoActual;
 		return null;
 	}
 
-	public int cuantasEnPrision( int[] filaDelJugador, int indiceJugador){
+	public boolean verificarLanzamientos( int[] lanzamientosJugadores ){
+		boolean valido = true;
+		for(int i = 0; i < lanzamientosJugadores.length; ++i){
+			if(lanzamientosJugadores[i] == 0){
+				valido = false;
+			}
+		}
+		return valido;
+	}
+
+	public int[] elMayor( int[] arreglo){
+		int[] resultados = {0,-1}; // resultados[0] = max del arreglo resultados[1] = indice del max
+		int max =0;
+		for(int i = 0; i < arreglo.length; ++i){
+			if ( arreglo[i] > max) {
+				max = arreglo[i];
+				resultados[0] = max;
+				resultados[1] = i;
+			}
+		}
+		return resultados;
+	}
+
+	public int[] elMenor( int[] arreglo){
+		int[] resultados = {100,-1}; // resultados[0] = max del arreglo resultados[1] = indice del max
+		int min = 100;
+		for(int i = 0; i < arreglo.length; ++i){
+			if ( arreglo[i] < min) {
+				min = arreglo[i];
+				resultados[0] = min;
+				resultados[1] = i;
+			}
+		}
+		return resultados;
+	}
+
+	public ArrayList<Integer> maxRepetido(int[] lanzamientosJugadores, int maximo, int indiceDelMax ){ //equivalente a Hay dos o mas valores iguales
+		ArrayList<Integer> indicesEmpatados = new ArrayList<>();
+		indicesEmpatados.clear(); //Lista vacia
+		boolean anadirIndiceMax = false;
+		for(int i = 0; i < lanzamientosJugadores.length; ++i){
+			if(i != indiceDelMax){
+				if(lanzamientosJugadores[i] == maximo){
+					indicesEmpatados.add(i);
+					anadirIndiceMax = true;
+				}
+			}
+		}
+		if(anadirIndiceMax){
+			indicesEmpatados.add(indiceDelMax);
+		}
+		return indicesEmpatados;
+	}
+
+	public int cuantasEnPrision( int[] filaDelJugador, int indiceJugador){ //Retorna un contador
+		//con el numero de piezas en prisión del jugador
 		int contador = 0;
 
 		for(int j = 0; j < filaDelJugador.length; ++j){ //Recorrer columnas de la fila
@@ -200,8 +246,115 @@ public class MiJugador extends Jugador {
 				++contador;
 			}
 		}
-
 		return contador;
+	}
+
+	public int cuantasFueraDeJuego( int[] filaDelJugador, int indiceJugador ){ //Cantidad de piezas del jugador que se encuentran en el tablero
+		//Sea en una casilla de la casa, del camino, o de la cárcel
+		int contador = 0;
+		for( int i = 0; i < filaDelJugador.length; ++i){
+			if(filaDelJugador[i] == 75){
+				++contador;
+			}
+		}
+		return contador;
+	}
+
+	public int piezasCorriendo(int[] filaDelJugador, int indiceJugador){
+		return 4 - (cuantasEnPrision(filaDelJugador, indiceJugador) - (cuantasFueraDeJuego(filaDelJugador, indiceJugador)));
+	}
+
+	public ArrayList<Integer> piezasValidos( int[] filaDelJugador, int indiceJugador ){
+		// Cuantas piezas estaban fuera de la cárcel pero sin llegar al final del tablero
+		// en un determinado momento (lo solemos usar antes de sacar de la cárcel )
+		ArrayList<Integer> validos = new ArrayList<>();
+		for( int i = 0; i<filaDelJugador.length; ++i){
+			if( (filaDelJugador[i] != carceles[indiceJugador]) && ( filaDelJugador[i] != 75) ){
+				validos.add(i);
+			}
+		}
+		return validos;
+	}
+
+	public ArrayList<Integer> sacarTodasDeLaCarcel( int[] filaDelJugador, int indiceJugador ){
+		// Saca todas las piezas de la cárcel
+		// retorna una lista indicando cuales piezas salieron
+		ArrayList<Integer> recienSalidos = new ArrayList<>();
+		while( cuantasEnPrision(filaDelJugador, indiceJugador) > 0 ){
+			int j = 0;
+			if( filaDelJugador[j] == carceles[indiceJugador] ){
+				filaDelJugador[j] = -carceles[indiceJugador];
+				recienSalidos.add(j);
+			}
+			++j;
+		}
+		return recienSalidos;
+	}
+
+	public ArrayList<Integer> sacarMaxDosDeLaCarcel( int[] filaDelJugador, int indiceJugador ){
+		// Saca todas las piezas de la cárcel
+		// retorna una lista indicando cuales piezas salieron
+		ArrayList<Integer> recienSalidos = new ArrayList<>();
+		int j = 0;
+		while( recienSalidos.size() < 3 && j < 4){
+			if(filaDelJugador[j] == carceles[indiceJugador]){
+				filaDelJugador[j] = -carceles[indiceJugador];
+				recienSalidos.add(j);
+			}
+			++j;
+		}
+
+
+		return recienSalidos;
+	}
+
+	public boolean pertenece( int[] arreglo, int valor){
+		boolean p = false;
+		for(int i = 0; i < arreglo.length; ++i){
+			if( arreglo[i] == valor){
+				p = true;
+			}
+		}
+		return true;
+	}
+
+	public ArrayList<Integer> esPosibleAsegurar( int[] filaDelJugador, int indiceJugador ){
+		//Recorremos la fila del jugador y si para algun valor P_i del arreglo el valor de uno de los dados
+		//o su suma, permite que la pieza con el valor P_i termine en una casilla segura se retorna
+		//cuales piezas pueden ser aseguradas (puede ser, 0, 1 o varias)
+		int sumaDados = puntajes[0] + puntajes[1];
+		//Tenemos un arreglo con casillas seguras, sea P_i el valor de la casilla y d_1 y d_2 los valores
+		//de los dados respectivamente, debemos veriticar si el arreglo de casillas seguras contiene a:
+		// P_i + d_1
+		// P_i + d_2
+		// P_i + d_1 + d_2
+		ArrayList<Integer> validos = piezasValidos(filaDelJugador, indiceJugador);
+		for( int i = 0; i < validos.size(); ++i){ //Para cada pieza valida verificar
+			int v1, v2, v3;
+			v1 = piezas[indiceJugador][validos.get(i)] + puntajes[0];
+			v2 = piezas[indiceJugador][validos.get(i)] + puntajes[1];
+			v3 = piezas[indiceJugador][validos.get(i)] + puntajes[0] + puntajes[1];
+			if( (pertenece(casillasSeguras, v1)) || pertenece(casillasSeguras, v2) || pertenece(casillasSeguras, v3) ){
+				
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<Integer> esPosibleCapturar( int[] filaDelJugador, int indiceJugador ){
+		return null;
+	}
+
+	public void moverPostCarcel( ArrayList<Integer> validas){ //Implementar movimiento, debe recibir una lista de piezas válidas para mover
+		//Este metodo se usa cuando se acaba de salir de la carcel y solo se podrá mover con uno de los indices
+		++contadorTurnos;
+	}
+	public void mover( ArrayList<Integer> validas ){ //Implementar movimiento, debe recibir una lista de piezas válidas para mover
+		if( paresAcumulados == 3){ // Cuando ocurra este evento, se escoge sacar la pieza más atrasada si no se puede asegurar una pieza con el puntaje
+
+
+		}
+		++contadorTurnos;
 	}
 
 }
